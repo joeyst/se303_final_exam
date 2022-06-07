e487228eefdde03e28e6de63512970ef21af86fd
joeyst <joeystenbeck@gmail.com>
2022-06-07 11:22:49 -0700

    Extract strings into methods.

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -2,9 +2,11 @@
 # To cure it, apply "extract function".
 
 def story
-  first = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
-  conclusion = "And that was it."
   first + middle + conclusion
+end
+
+def first
+  "Mr. Dursley was the director of a firm called Grunnings, which made drills."
 end
 
 def middle
@@ -15,4 +17,8 @@ def middle
   middle << "\n"
   middle << "\n"
   middle
+end
+
+def conclusion
+  "And that was it."
 end
