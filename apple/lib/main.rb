ac455f9409d1d3f19a641389466978d6c99e5f03
joeyst <joeystenbeck@gmail.com>
2022-06-07 10:43:29 -0700

    Use add_new_lines inline.

--- apple/lib/main.rb
+++ apple/lib/main.rb
@@ -3,12 +3,7 @@
 
 def story
   first = "Mr. Dursley was the director of a firm called Grunnings, which made drills."
-  middle = ""
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
-  middle << "\n"
+  middle = add_new_lines
   conclusion = "And that was it."
   first + middle + conclusion
 end
